b5dc95d9d1c7871303dc3542a2591577dfaf4226
Jake Scott <bemybanker@gmail.com>
2019-09-24 10:15:49 -0500

    got nav working, and other paths sorted

--- hrapp/models/computer.py
+++ hrapp/models/computer.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Computer(models.Model):
     '''
--- hrapp/models/employee.py
+++ hrapp/models/employee.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 # from .department import Department
 
 class Employee(models.Model):
