c86867963d0e8d7f761708f2c25cfea94ff30b9b
Michael Drew Palazola <mpalazola@hotmail.com>
2019-09-24 11:00:10 -0500

    added the program list initial view and navbar link

--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -2,3 +2,4 @@ from .employees.employee_list import employee_list
 from .home import home
 from .auth.logout import logout_user
 from .connection import Connection
+from .trainingprograms.training_program_list import training_program_list
